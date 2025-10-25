5ec52fc77b47173ede59808b19d43c2996b7a70a
David Vergano <verganodavid29@gmail.com>
2025-10-25 02:51:58 +0000

    Create API for converting bytes to messages
    Will likely have to make variables/constants of tftpBytes public.

--- tftpBytes.go
+++ tftpBytes.go
@@ -1,16 +1,16 @@
 package main
 
 import (
-	"net"
+	"errors"
+	"io"
+	"slices"
 	"strings"
-	"time"
 )
 
-const (
-	logEventNewSession = iota
-	logEventEndSession
-	logEventErrorSession
-)
+var ErrUnknownOpcode = errors.New("Unknown opcode found when decoding message")
+var ErrUnknownMessage = errors.New("Unknown message type used to encode message")
+var ErrShortMessage = errors.New("Impossibly short message received")
+var ErrUnterminatedNullString = errors.New("Null byte not found")
 
 const (
 	opcodeReadByte = 1 + iota
@@ -32,6 +32,13 @@ const (
 	errorCodeNoSuchUser
 )
 
+/*
+ * Structure of Read Message in byte is:
+ * 2 byte opcode = 0x0001
+ * string (null terminated sequence of bytes) filename
+ * string (null terminated sequence of bytes) mode
+ * [many](key string, value string) optionsAsKeyValuePairs
+ */
 type readMessage struct {
 	filename string
 	mode     string
@@ -42,6 +49,13 @@ func newReadMessage(filename string, mode string, options map[string]string) rea
 	return readMessage{filename, mode, options}
 }
 
+/*
+ * Structure of Write Message in bytes is:
+ * 2 byte opcode = 0x0002
+ * string (null terminated sequence of bytes) filename
+ * string (null terminated sequence of bytes) mode
+ * [many](key string, value string) optionsAsKeyValuePairs
+ */
 type writeMessage struct {
 	filename string
 	mode     string
@@ -52,6 +66,12 @@ func newWriteMessage(filename string, mode string, options map[string]string) wr
 	return writeMessage{filename, mode, options}
 }
 
+/*
+ * Structure of Data Message in bytes is:
+ * 2 byte opcode = 0x0003
+ * 2 byte blockNumber
+ * [many]char fileDataItself
+ */
 type dataMessage struct {
 	blockNumber uint16
 	body        []byte
@@ -61,6 +81,11 @@ func newDataMessage(blockNumber uint16, body []byte) dataMessage {
 	return dataMessage{blockNumber, body}
 }
 
+/*
+ * Structure of Acknowledge Message in bytes is:
+ * 2 byte opcode = 0x0004
+ * 2 byte acknowledgedBlockNumber
+ */
 type acknowledgeMessage struct {
 	blockNumber uint16
 }
@@ -69,6 +94,12 @@ func newAcknowledgeMessage(blockNumber uint16) acknowledgeMessage {
 	return acknowledgeMessage{blockNumber}
 }
 
+/*
+ * Structure of Error Message in bytes is:
+ * 2 byte opcode = 0x0005
+ * 2 byte errorCode
+ * string (null terminated sequence of bytes) humanReadableErrorMessage
+ */
 type errorMessage struct {
 	errorCode   uint16
 	explanation string
@@ -78,82 +109,17 @@ func newErrorMessage(errorCode uint16, explanation string) errorMessage {
 	return errorMessage{errorCode, explanation}
 }
 
+/*
+ * Structure of Option Acknowledgement Message in bytes is:
+ * 2 byte opcode = 0x0006
+ * [many](key string, value string) optionsAsKeyValuePairs
+ */
 type optionAcknowledgeMessage struct {
 	options map[string]string
 }
 
 func newOptionAcknowledgeMessage(options map[string]string) optionAcknowledgeMessage {
 	return optionAcknowledgeMessage{options}
-}
-
-type tftpState struct {
-	// set before connection established
-	destination net.UDPAddr
-
-	// set when connection established
-	opcode   uint16
-	filename string
-	mode     string
-	options  map[string]string
-
-	// values determined by options
-	blockSize    uint16
-	timeout      time.Duration
-	transferSize uint64
-	windowSize   uint16
-
-	// updated upon acknowledgements
-	blockNumber           uint16
-	totalBytesTransferred uint32
-}
-
-func newTftpState(destination net.UDPAddr, message string) (tftpState, error) {
-	var state tftpState
-
-	state.blockSize = 512
-	state.timeout = time.Second * 5
-	state.transferSize = 0 // 0 if unknown
-	state.windowSize = 512
-	state.options = make(map[string]string)
-
-	// handle client iniating read request / write request
-	/*
-	 * 2 bytes for opcode
-	 * 1 byte minimum for filename
-	 * 1 byte null terminator for filename
-	 * 5 byte minimum for mode ("octet" is 5 bytes, "mail" is obselete according to RFC 1350)
-	 * 1 byte null terminator for filename
-	 */
-	initialRequestMinLength := 2 + 1 + 1 + 5 + 1
-	totalMessageLength := len(message)
-	currentMessagePosition := 0
-
-	if totalMessageLength < initialRequestMinLength {
-		// error
-	}
-	state.opcode = uint16(message[0])
-	currentMessagePosition++
-	state.filename = string(message[currentMessagePosition:])
-	currentMessagePosition += len(state.filename) + 1
-	if currentMessagePosition >= totalMessageLength {
-		// error
-	}
-	state.mode = strings.ToLower(string(message[currentMessagePosition:]))
-	currentMessagePosition += len(state.mode) + 1
-	for currentMessagePosition < totalMessageLength {
-		optionKey := string(message[currentMessagePosition:])
-		currentMessagePosition += len(optionKey) + 1
-
-		if currentMessagePosition >= totalMessageLength {
-			// error
-		}
-		optionVal := string(message[currentMessagePosition:])
-		currentMessagePosition += len(optionVal) + 1
-
-		state.options[optionKey] = optionVal
-	}
-
-	return state, nil
 }
 
 func MessageAsBytes(message any, buf *[]byte) error {
@@ -171,19 +137,27 @@ func MessageAsBytes(message any, buf *[]byte) error {
 	case optionAcknowledgeMessage:
 		optionAcknowledgeMessageAsBytes(message.(optionAcknowledgeMessage), buf)
 	default:
-		// error
+		return ErrUnknownMessage
 	}
 
 	return nil
 }
 
-/*
- * Structure of Read Message in byte is:
- * 2 byte opcode = 0x0001
- * string (null terminated sequence of bytes) filename
- * string (null terminated sequence of bytes) mode
- * [many](key string, value string) optionsAsKeyValuePairs
- */
+func popNullString(buf *[]byte) (string, error) {
+	nullBytePos := slices.Index(*buf, 0)
+	if nullBytePos < 0 || nullBytePos >= len(*buf) {
+		return "", ErrUnterminatedNullString
+	}
+	nullString := string((*buf)[:nullBytePos])
+	if nullBytePos+1 >= len(*buf) {
+		*buf = (*buf)[:0]
+		return nullString, io.EOF
+	}
+	*buf = (*buf)[nullBytePos+1:]
+
+	return nullString, nil
+}
+
 func readMessageAsBytes(message readMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -205,13 +179,6 @@ func readMessageAsBytes(message readMessage, buf *[]byte) error {
 	return nil
 }
 
-/*
- * Structure of Write Message in bytes is:
- * 2 byte opcode = 0x0002
- * string (null terminated sequence of bytes) filename
- * string (null terminated sequence of bytes) mode
- * [many](key string, value string) optionsAsKeyValuePairs
- */
 func writeMessageAsBytes(message writeMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -233,12 +200,6 @@ func writeMessageAsBytes(message writeMessage, buf *[]byte) error {
 	return nil
 }
 
-/*
- * Structure of Data Message in bytes is:
- * 2 byte opcode = 0x0003
- * 2 byte blockNumber
- * [many]char fileDataItself
- */
 func dataMessageAsBytes(message dataMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -255,11 +216,6 @@ func dataMessageAsBytes(message dataMessage, buf *[]byte) error {
 	return nil
 }
 
-/*
- * Structure of Acknowledge Message in bytes is:
- * 2 byte opcode = 0x0004
- * 2 byte acknowledgedBlockNumber
- */
 func acknowledgeMessageAsBytes(message acknowledgeMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -273,12 +229,6 @@ func acknowledgeMessageAsBytes(message acknowledgeMessage, buf *[]byte) error {
 	return nil
 }
 
-/*
- * Structure of Error Message in bytes is:
- * 2 byte opcode = 0x0005
- * 2 byte errorCode
- * string (null terminated sequence of bytes) humanReadableErrorMessage
- */
 func errorMessageAsBytes(message errorMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -293,15 +243,9 @@ func errorMessageAsBytes(message errorMessage, buf *[]byte) error {
 	*buf = append(*buf, message.explanation...)
 	*buf = append(*buf, '\x00')
 
-	// send as byte[]
 	return nil
 }
 
-/*
- * Structure of Option Acknowledgement Message in bytes is:
- * 2 byte opcode = 0x0006
- * [many](key string, value string) optionsAsKeyValuePairs
- */
 func optionAcknowledgeMessageAsBytes(message optionAcknowledgeMessage, buf *[]byte) error {
 	// zero out length, keep capacity
 	*buf = (*buf)[:0]
@@ -314,6 +258,219 @@ func optionAcknowledgeMessageAsBytes(message optionAcknowledgeMessage, buf *[]by
 		*buf = append(*buf, []byte(k+"\x00"+v+"\x00")...)
 	}
 
-	// send as byte[]
 	return nil
+}
+
+func BytesAsMessage(buf []byte) (any, error) {
+	var message any
+	var opcode byte
+	var err error
+
+	if len(buf) < 2 {
+		return nil, ErrShortMessage
+	}
+
+	// leading byte of opcode is always 0x00
+	if buf[0] != 0 {
+		return nil, ErrUnknownOpcode
+	}
+
+	// 2nd byte of opcode is what determines message types
+	opcode = buf[1]
+	buf = buf[2:]
+	switch opcode {
+	case opcodeReadByte:
+		message, err = bytesAsReadMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	case opcodeWriteByte:
+		message, err = bytesAsWriteMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	case opcodeDataByte:
+		message, err = bytesAsDataMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	case opcodeAcknowledgeByte:
+		message, err = bytesAsAcknowledgeMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	case opcodeErrorByte:
+		message, err = bytesAsErrorMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	case opcodeOptionAcknowledgeByte:
+		message, err = bytesAsOptionAcknowledgeMessage(buf)
+		if err != nil {
+			return nil, err
+		}
+	default:
+		return nil, ErrUnknownOpcode
+	}
+
+	return message, nil
+}
+
+func bytesAsReadMessage(buf []byte) (readMessage, error) {
+	var filename string
+	var mode string
+	var options map[string]string = make(map[string]string)
+
+	minPossibleLen := 1 + 1 + 5 + 1
+
+	if len(buf) < minPossibleLen {
+		return readMessage{}, ErrShortMessage
+	}
+
+	filename, err := popNullString(&buf)
+	if err != nil {
+		return readMessage{}, ErrUnterminatedNullString
+	}
+
+	mode, err = popNullString(&buf)
+	if err != nil {
+		return readMessage{}, ErrUnterminatedNullString
+	}
+	mode = strings.ToLower(mode)
+
+	for len(buf) > 0 {
+		key, err := popNullString(&buf)
+		if err != nil {
+			return readMessage{}, ErrUnterminatedNullString
+		}
+
+		val, err := popNullString(&buf)
+		if err != nil && err != io.EOF {
+			return readMessage{}, ErrUnterminatedNullString
+		}
+
+		options[key] = val
+	}
+
+	return newReadMessage(filename, mode, options), nil
+}
+
+func bytesAsWriteMessage(buf []byte) (writeMessage, error) {
+	var filename string
+	var mode string
+	var options map[string]string = make(map[string]string)
+
+	minPossibleLen := 1 + 1 + 5 + 1
+
+	if len(buf) < minPossibleLen {
+		return writeMessage{}, ErrShortMessage
+	}
+
+	filename, err := popNullString(&buf)
+	if err != nil {
+		return writeMessage{}, ErrUnterminatedNullString
+	}
+
+	mode, err = popNullString(&buf)
+	if err != nil {
+		return writeMessage{}, ErrUnterminatedNullString
+	}
+	mode = strings.ToLower(mode)
+
+	for len(buf) > 0 {
+		key, err := popNullString(&buf)
+		if err != nil {
+			return writeMessage{}, ErrUnterminatedNullString
+		}
+
+		val, err := popNullString(&buf)
+		if err != nil && err != io.EOF {
+			return writeMessage{}, ErrUnterminatedNullString
+		}
+
+		options[key] = val
+	}
+
+	return newWriteMessage(filename, mode, options), nil
+}
+
+func bytesAsDataMessage(buf []byte) (dataMessage, error) {
+	var blockNumber uint16
+	var data []byte
+
+	minPossibleLen := 2 + 1
+
+	if len(buf) < minPossibleLen {
+		return dataMessage{}, ErrShortMessage
+	}
+
+	blockNumber += uint16(buf[0]) << 8
+	blockNumber += uint16(buf[1])
+
+	data = buf[2:]
+
+	return newDataMessage(blockNumber, data), nil
+}
+
+func bytesAsAcknowledgeMessage(buf []byte) (acknowledgeMessage, error) {
+	var blockNumber uint16
+
+	minPossibleLen := 2
+
+	if len(buf) < minPossibleLen {
+		return acknowledgeMessage{}, ErrShortMessage
+	}
+
+	blockNumber += uint16(buf[0]) << 8
+	blockNumber += uint16(buf[1])
+
+	return newAcknowledgeMessage(blockNumber), nil
+}
+
+func bytesAsErrorMessage(buf []byte) (errorMessage, error) {
+	var errorCode uint16
+	var explanation string
+
+	minPossibleLen := 1 + 1 + 5 + 1
+
+	if len(buf) < minPossibleLen {
+		return errorMessage{}, ErrShortMessage
+	}
+
+	errorCode += uint16(buf[0]) << 8
+	errorCode += uint16(buf[1])
+
+	buf = buf[2:]
+	explanation, err := popNullString(&buf)
+	if err != nil && err != io.EOF {
+		return errorMessage{}, ErrUnterminatedNullString
+	}
+
+	return newErrorMessage(errorCode, explanation), nil
+}
+
+func bytesAsOptionAcknowledgeMessage(buf []byte) (optionAcknowledgeMessage, error) {
+	var options map[string]string = make(map[string]string)
+
+	minPossibleLen := 1 + 1 + 1 + 1
+
+	if len(buf) < minPossibleLen {
+		return optionAcknowledgeMessage{}, ErrShortMessage
+	}
+
+	for len(buf) > 0 {
+		key, err := popNullString(&buf)
+		if err != nil {
+			return optionAcknowledgeMessage{}, ErrUnterminatedNullString
+		}
+
+		val, err := popNullString(&buf)
+		if err != nil && err != io.EOF {
+			return optionAcknowledgeMessage{}, ErrUnterminatedNullString
+		}
+
+		options[key] = val
+	}
+
+	return newOptionAcknowledgeMessage(options), nil
 }
--- tftpBytes_test.go
+++ tftpBytes_test.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bytes"
+	"reflect"
 	"testing"
 )
 
@@ -14,13 +15,13 @@ func TestReadMessageAsBytes(t *testing.T) {
 	options[key1] = val1
 	message := newReadMessage(filename, mode, options)
 
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	output := []byte{0, opcodeReadByte, 'f', 'i', 'l', 'e', 0, 'o', 'c', 't', 'a', 'l', 0, 's', 'y', 'n', 't', 'a', 'x', 0, 'o', 'n', 0}
+
 	err := readMessageAsBytes(message, &buf)
 	if err != nil {
 		t.Errorf("Some argument passed to tftpReadRequest is nil")
 	}
-
 	if !bytes.Equal(output, buf) {
 		t.Errorf("%v != %v\n", output, buf)
 		return
@@ -36,7 +37,7 @@ func TestWriteMessageAsBytes(t *testing.T) {
 	options[key1] = val1
 	message := newWriteMessage(filename, mode, options)
 
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	output := []byte{0, opcodeWriteByte, 'f', 'i', 'l', 'e', 0, 'n', 'e', 't', 'a', 's', 'c', 'i', 'i', 0, 's', 'y', 'n', 't', 'a', 'x', 0, 'o', 'n', 0}
 	err := writeMessageAsBytes(message, &buf)
 	if err != nil {
@@ -53,14 +54,13 @@ func TestDataMessageAsBytes(t *testing.T) {
 	data := []byte{5, 4, 3, 2, 1}
 	message := newDataMessage(blockNumber, data)
 
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	output := []byte{0, opcodeDataByte, byte(blockNumber >> 8), byte(blockNumber % 0x100)}
 	output = append(output, data...)
 	err := dataMessageAsBytes(message, &buf)
 	if err != nil {
 		t.Errorf("Some argument passed to tftpDta is nil")
 	}
-
 	if !bytes.Equal(output, buf) {
 		t.Errorf("%v != %v\n", output, buf)
 		return
@@ -71,13 +71,12 @@ func TestAcknowledgeMessageAsBytes(t *testing.T) {
 	var blockNumber uint16 = 0xabcd
 	message := newAcknowledgeMessage(blockNumber)
 
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	output := []byte{0, opcodeAcknowledgeByte, byte(blockNumber >> 8), byte(blockNumber % 0x100)}
 	err := acknowledgeMessageAsBytes(message, &buf)
 	if err != nil {
 		t.Errorf("Some argument passed to tftpAcknowledge is nil")
 	}
-
 	if !bytes.Equal(output, buf) {
 		t.Errorf("%v != %v\n", output, buf)
 		return
@@ -89,7 +88,7 @@ func TestErrorMessageAsBytes(t *testing.T) {
 	explanation := "among us"
 	message := newErrorMessage(errorCode, explanation)
 
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	output := []byte{0, opcodeErrorByte, 0, byte(errorCode)}
 	output = append(output, explanation...)
 	output = append(output, '\x00')
@@ -97,7 +96,6 @@ func TestErrorMessageAsBytes(t *testing.T) {
 	if err != nil {
 		t.Errorf("Some argument passed to tftpAcknowledge is nil")
 	}
-
 	if !bytes.Equal(output, buf) {
 		t.Errorf("%v != %v\n", output, buf)
 		return
@@ -109,7 +107,7 @@ func TestOptionAcknowledgeMessageAsBytes(t *testing.T) {
 	key1 := "syntax"
 	val1 := "on"
 	options[key1] = val1
-	buf := make([]byte, 0xffff)
+	buf := make([]byte, 0, 0xffff)
 	message := newOptionAcknowledgeMessage(options)
 
 	output := []byte{0, opcodeOptionAcknowledgeByte}
@@ -119,9 +117,151 @@ func TestOptionAcknowledgeMessageAsBytes(t *testing.T) {
 	if err != nil {
 		t.Errorf("Some argument passed to tftpAcknowledge is nil")
 	}
-
 	if !bytes.Equal(output, buf) {
 		t.Errorf("%v != %v\n", output, buf)
+		return
+	}
+}
+
+func TestBytesAsReadMessage(t *testing.T) {
+	filename := "vals.zip"
+	mode := "octal"
+	options := make(map[string]string)
+	key1 := "backup"
+	val1 := "true"
+	options[key1] = val1
+	buf := make([]byte, 0, 0xffff)
+	buf = append(buf, []byte(filename)...)
+	buf = append(buf, 0)
+	buf = append(buf, []byte(mode)...)
+	buf = append(buf, 0)
+	for k, v := range options {
+		buf = append(buf, []byte(k)...)
+		buf = append(buf, 0)
+		buf = append(buf, []byte(v)...)
+		buf = append(buf, 0)
+	}
+	expected := newReadMessage(filename, mode, options)
+
+	message, err := bytesAsReadMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsReadMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected readMessage{%v, %v, %v} != message readMessage{%v, %v, %v}\n", expected.filename, expected.mode, expected.options, message.filename, message.mode, message.options)
+		return
+	}
+}
+
+func TestBytesAsWriteMessage(t *testing.T) {
+	filename := "vals.zip"
+	mode := "octal"
+	options := make(map[string]string)
+	key1 := "backup"
+	val1 := "true"
+	options[key1] = val1
+	buf := make([]byte, 0, 0xffff)
+	buf = append(buf, []byte(filename)...)
+	buf = append(buf, 0)
+	buf = append(buf, []byte(mode)...)
+	buf = append(buf, 0)
+	for k, v := range options {
+		buf = append(buf, []byte(k)...)
+		buf = append(buf, 0)
+		buf = append(buf, []byte(v)...)
+		buf = append(buf, 0)
+	}
+	expected := newWriteMessage(filename, mode, options)
+
+	message, err := bytesAsWriteMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsWriteMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected writeMessage{%v, %v, %v} != message writeMessage{%v, %v, %v}\n", expected.filename, expected.mode, expected.options, message.filename, message.mode, message.options)
+		return
+	}
+}
+
+func TestBytesAsDataMessage(t *testing.T) {
+	var blockNumber uint16 = 0x5678
+	data := []byte{9, 8, 7, 6, 5, 15, 14, 13, 12, 11}
+	buf := make([]byte, 0, 0xffff)
+	buf = append(buf, byte(blockNumber>>8), byte(blockNumber))
+	buf = append(buf, data...)
+	expected := newDataMessage(blockNumber, data)
+
+	message, err := bytesAsDataMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsDataMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected dataMessage{%v, %v} != message dataMessage{%v, %v}\n", expected.blockNumber, expected.body, message.blockNumber, message.body)
+		return
+	}
+}
+
+func TestBytesAsAcknowledgeMessage(t *testing.T) {
+	var blockNumber uint16 = 0x5678
+	buf := make([]byte, 0, 0xffff)
+	buf = append(buf, byte(blockNumber>>8), byte(blockNumber))
+	expected := newAcknowledgeMessage(blockNumber)
+
+	message, err := bytesAsAcknowledgeMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsAcknowledgeMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected acknowledgeMessage{%v} != message acknowledgeMessage{%v}\n", expected.blockNumber, message.blockNumber)
+		return
+	}
+}
+
+func TestBytesAsErrorMessage(t *testing.T) {
+	var errorCode uint16 = errorCodeTooMuchData
+	explanation := "You asked for too much data"
+	buf := make([]byte, 0, 0xffff)
+	buf = append(buf, byte(errorCode>>8), byte(errorCode))
+	buf = append(buf, []byte(explanation)...)
+	buf = append(buf, 0)
+	expected := newErrorMessage(errorCode, explanation)
+
+	message, err := bytesAsErrorMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsErrorMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected errorMessage{%v, %v} != message errorMessage{%v, %v}\n", expected.errorCode, expected.explanation, message.errorCode, message.explanation)
+		return
+	}
+}
+
+func TestBytesAsOptionAcknowledgeMessage(t *testing.T) {
+	options := make(map[string]string)
+	key1 := "backup"
+	val1 := "true"
+	options[key1] = val1
+	buf := make([]byte, 0, 0xffff)
+	for k, v := range options {
+		buf = append(buf, []byte(k)...)
+		buf = append(buf, 0)
+		buf = append(buf, []byte(v)...)
+		buf = append(buf, 0)
+	}
+	expected := newOptionAcknowledgeMessage(options)
+
+	message, err := bytesAsOptionAcknowledgeMessage(buf)
+	if err != nil {
+		t.Errorf("Some error occured when bytesAsOptionAcknowledgeMessage tried parsing buf\n")
+		return
+	}
+	if !reflect.DeepEqual(expected, message) {
+		t.Errorf("expected optionAcknowledgeMessage{%v} != message optionAcknowlegeMessage{%v}\n", expected.options, message.options)
 		return
 	}
 }
